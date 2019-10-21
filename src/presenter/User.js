0da9cdb7989c9279c374f6477f81bf40d5c0f28a
H. B. Brooks <hbrooks3>
2019-10-20 20:33:18 -0500

    added basic hook

---
+++ src/presenter/User.js
@@ -0,0 +1,12 @@
+export default function UseUser() {
+  const exampleUser = {
+    userName: "bigGuy",
+    email: "lifter1@gmail.com",
+    firstName: "Phil",
+    lastName: "Myez",
+    height: "6'4\"",
+    weight: "800lbs",
+  }
+
+  return exampleUser;
+}
--- src/view/ProfilePage.js
+++ src/view/ProfilePage.js
@@ -1,9 +1,12 @@
 import React from "react";
+import UseUser from "../presenter/User";
 
 export default function ProfilePage(props) {
+  const user = UseUser();
+
   return (
     <div>
-      <h2>Profile</h2>
+      <h2>{user.firstName + " " + user.lastName}</h2>
     </div>
   );
 }
