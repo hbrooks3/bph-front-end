58f75337b349fea00c2ab8c5507afb69b1ceb4f7
gdarin <gdarin@wisc.edu>
2019-10-24 18:18:47 -0500

    test image

--- src/view/HomePage.js
+++ src/view/HomePage.js
@@ -1,15 +1,23 @@
 import React from "react";
-/*
+
 import Image from "react-bootstrap/image";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-*/
+import Squat from "./squatTest.jpg";
+
 
 export default function HomePage(props) {
   return (
     <div>
       <h2>Home</h2>
+      <Container>
+        <Row>
+          <Col>
+            <Image src={Squat} rounded />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
