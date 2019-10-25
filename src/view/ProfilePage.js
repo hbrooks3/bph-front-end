71356d1af158a6856e1292e131d1e8cf10062341
H. B. Brooks <hbrooks3>
2019-10-24 19:00:40 -0500

    added profile info

--- src/view/ProfilePage.js
+++ src/view/ProfilePage.js
@@ -4,6 +4,7 @@ import useUser from "../dummy-presenter/User";
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import Spinner from 'react-bootstrap/Spinner'
 
 export default function ProfilePage(props) {
@@ -19,14 +20,32 @@ export default function ProfilePage(props) {
       {!isLoading && user && (
         <Container>
           <Col sm={4}>
-            <Card>
-              <Card.Title>
-                General Information
-              </Card.Title>
-              <Card.Body>
-                Height: {user.height}    
-              </Card.Body>    
-            </Card>
+            <Row>
+              <Card className="text-center">
+                <Card.Title>
+                  General Information
+                </Card.Title>
+                <Card.Body>
+                  {`Height: ${user.height}
+                  Weight: ${user.weight}
+                  Age: ${user.age}`}
+                </Card.Body>
+              </Card>
+            </Row>
+            <Row>
+              <Card className="text-center">
+                <Card.Title>
+                  General Information
+                </Card.Title>
+                <Card.Body>
+                  <Card.Text>Height: {user.height}</Card.Text>
+                  <Card.Text>Height: {user.height}</Card.Text>
+                  <Card.Text>Height: {user.height}</Card.Text>
+                </Card.Body>    
+              </Card>
+
+            </Row>
+            
           </Col>
           <Col>
           </Col>
