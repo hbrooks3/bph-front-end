831b987c9921d40e39b26fa987b3bb89102ca9dd
H. B. Brooks <hbrooks3>
2019-10-31 19:49:01 -0500

    implemented login

--- src/view/Navbar.js
+++ src/view/Navbar.js
@@ -5,113 +5,140 @@ import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
+import ReactDOM from 'react-dom';
 
 // Presenter imports
-import { createAccount } from "../dummy-presenter/User";
-// import { createAccount } from "../presenter/User";
+import { createAccount, loginUser } from "../dummy-presenter/User";
+// import { createAccount, loginUser } from "../presenter/User";
 
 export default function NavBar() {
-    const [show, setShow] = useState(false);
-    const handleCloseLogin = () => setShow(false);
-    const handleShowLogin = () => setShow(true);
+  const [loginEmail, setLoginEmail] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+  const [registerEmail, setRegisterEmail] = useState('');
+  const [registerPassword, setRegisterPassword] = useState('');
 
-    const [display, setDisplay] = useState(false);
-    const handleCloseAccount = () => setDisplay(false);
-    const handleShowAccount = () => setDisplay(true);
+  const [showLogin, setShowLogin] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
 
-
+  const closeLoginModal = () => setShowLogin(false);
+  const openLoginModal = () => setShowLogin(true);
+  const closeRegisterModal = () => setShowRegister(false);
   
-    return (
+  const openRegisterModal = () => {
+    setShowLogin(false);
+    setShowRegister(true);
+  }
 
-    <>
-        <Navbar bg="primary" variant="dark" expand="lg">
-            <Navbar.Brand href="/">
-            {`Badger Powerlifting Hub`}
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/plans">Plans</Nav.Link>
-                <Nav.Link href="/profile">Profile</Nav.Link>
-            </Nav>
-            <Form inline>
-              <Button variant="primary" onClick={handleShowLogin}>Login</Button>
-            </Form>
-            </Navbar.Collapse>
-        </Navbar>
+  const handleLogin = () => {
+    closeLoginModal();
+    loginUser(loginEmail, loginPassword);
+  }
 
-      <Modal show={show} onHide={handleCloseLogin}>
-        <Modal.Header closeButton>
-          <Modal.Title>Login</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>  
-          <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Nav.Item>
-              <Nav.Link eventKey="link-1" onClick={onNeedAccountClick}>Don't have an account? Create one here</Nav.Link>
-            </Nav.Item>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="primary" onClick={handleCloseLogin}>
-            Login
-          </Button>
-          <Button variant="secondary" onClick={handleCloseLogin}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>   
+  const handleRegister = () => {
+    createAccount(registerEmail, registerPassword);
+    closeRegisterModal();
+    openLoginModal();
+  }
+  
+  return (
+  <>
+    <Navbar bg="primary" variant="dark" expand="lg">
+      <Navbar.Brand href="/">
+      {`Badger Powerlifting Hub`}
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/plans">Plans</Nav.Link>
+        <Nav.Link href="/profile">Profile</Nav.Link>
+      </Nav>
+      <Form inline>
+        <Button variant="primary" onClick={openLoginModal}>Login</Button>
+      </Form>
+      </Navbar.Collapse>
+    </Navbar>
 
-      <Modal show={display} onHide={handleCloseAccount}>
-        <Modal.Header closeButton>
-          <Modal.Title>Create Account</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>  
-          <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="primary" onClick={onCreateAccountClick}>
-            Create
-          </Button>
-          <Button variant="secondary" onClick={handleCloseAccount}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal> 
-    </>
-    )
+    <Modal show={showLogin} onHide={closeLoginModal}>
+      <Modal.Header closeButton>
+        <Modal.Title>Login</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>  
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              value={loginEmail}
+              onChange={event => setLoginEmail(event.target.value)}
+              placeholder="Enter email"
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={loginPassword}
+              onChange={event => setLoginPassword(event.target.value)}
+            />
+          </Form.Group>
+          <Nav.Item>
+            <Nav.Link eventKey="link-1" onClick={openRegisterModal}>Don't have an account? Create one here</Nav.Link>
+          </Nav.Item>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="primary" onClick={handleLogin}>
+          Login
+        </Button>
+        <Button variant="secondary" onClick={closeLoginModal}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>   
 
-
-    function onNeedAccountClick(event){
-      handleShowAccount();
-      handleCloseLogin();
-    }
-
-    function onCreateAccountClick(event){
-      handleCloseAccount();
-      handleShowLogin();
-    }
+    <Modal show={showRegister} onHide={closeRegisterModal}>
+      <Modal.Header closeButton>
+        <Modal.Title>Create Account</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>  
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={registerEmail}
+              onChange={event => setRegisterEmail(event.target.value)}
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={registerPassword}
+              onChange={event => setRegisterPassword(event.target.value)}
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="primary" onClick={handleRegister}>
+          Create
+        </Button>
+        <Button variant="secondary" onClick={closeRegisterModal}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal> 
+  </>
+  )
 }
