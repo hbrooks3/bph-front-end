989e42874a6305c571ec70ce8919d113434173c5
H. B. Brooks <hbrooks3>
2019-11-01 14:05:04 -0500

    Added all avialiable API calls

--- src/presenter/User.js
+++ src/presenter/User.js
@@ -2,45 +2,121 @@ import { useState, useEffect } from 'react';
 import { ROOT } from './Root';
 
 export function createAccount(email, password) {
-  const apiRoute = ROOT + '/User/CreateAccount'
+  const apiRoute = ROOT + '/User/CreateUser'
 
   fetch(
     apiRoute,
     {
       method: 'POST',
-      body: JSON.stringify({email: email, password: password}),
+      body: JSON.stringify({Email: email, Password: password}),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      credentials: 'include',
     },
   );
 }
 
 export function loginUser(email, password) {
-  const apiRoute = ROOT + '/User/LoginUser'
+  const apiRoute = ROOT + '/User/LogoutUser'
 
   fetch(
     apiRoute,
     {
       method: 'POST',
-      body: JSON.stringify({email: email, password: password}),
+      body: JSON.stringify({Email: email, Password: password}),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      credentials: 'include',
     },
   );
+}
+
+//TODO: Check if header is needed
+export function logoutUser() {
+  const apiRoute = ROOT + '/User/LogoutUser'
+
+  fetch(
+    apiRoute,
+    {
+      method: 'POST',
+      // headers: {
+      //   'Content-Type': 'application/json',
+      //   'Accept': 'application/json'
+      // },
+      credentials: 'include',
+    },
+  );
+}
+
+export function updateUser(user) {
+  const apiRoute = ROOT + '/User/UpdateUser'
+
+  fetch(
+    apiRoute,
+    {
+      method: 'PUT',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      credentials: 'include',
+    },
+  );
+}
+
+export function useSession() {
+  const [sessionStatus, setSessionStatus] = useState(false);
+
+  const apiRoute = ROOT + '/User/CheckSessionStatus';
+
+  useEffect(() => {
+    fetch(
+      apiRoute,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        credentials: 'include',
+      },
+    ).then((response) => {
+      console.log("FETCH!");
+      return response.json();
+    }).then((data) => {
+      setSessionStatus(data)
+    }).catch((error) => {
+      console.log(error);
+    });
+  }, [apiRoute]);
+
+  return sessionStatus;
 }
 
 export function useUser() {
   const [isLoading, setIsLoading] = useState(true);
   const [user, setUser] = useState(null);
 
-  // const apiAddress = `${Root}/api/User/GetUser/`;
-  
-  // https://jsonplaceholder.typicode.com/
-  const apiAddress = 'https://jsonplaceholder.typicode.com/posts/1';
+  const apiRoute = ROOT + '/User/GetCurrentUser';
 
   useEffect(() => {
     fetch(
-      apiAddress,
+      apiRoute,
       {
         method: `GET`,
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        credentials: 'include',
       },
     ).then((response) => {
+      console.log("FETCH!");
       return response.json();
     }).then((data) => { 
       console.log(data);
@@ -49,7 +125,7 @@ export function useUser() {
     }).catch((error) => {
       console.log(error);
     });
-  }, [apiAddress]);
+  }, [apiRoute]);
 
   return { user, isLoading };
 }
