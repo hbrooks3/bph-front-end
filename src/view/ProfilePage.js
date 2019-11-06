3a31a0e4f37d5a0a21f2760281f380f228c7de94
H. B. Brooks <hbrooks3>
2019-11-06 13:44:32 -0600

    Pulled out DisplayCard Component

--- src/view/ProfilePage.js
+++ src/view/ProfilePage.js
@@ -1,12 +1,12 @@
 // React imports
 import React, {useState} from "react";
-import Card from 'react-bootstrap/Card';
+
+// Bootstrap Imports
 import CardColumns from 'react-bootstrap/CardColumns';
-import Col from 'react-bootstrap/Col';
 import Spinner from 'react-bootstrap/Spinner';
-import Button from 'react-bootstrap/Button';
-import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
-import Form from 'react-bootstrap/Form';
+
+// Custom Components
+import EditableCard from './EditableCard';
 
 // Presenter imports
 import { useUser, updateUser } from "../dummy-presenter/User";
@@ -33,102 +33,49 @@ function PageBody(props) {
 
   const [user, editUser] = useState(initialUser);
 
-  const pushUpdate = (event) => {
-    event.preventDefault();
+  const pushUpdate = () => {
     updateUser(user);
   };
 
-  // Updates contians update functions for each 
-  const controls = Object.keys(user).reduce((accumulator, key) => {
-    accumulator.update[key] = (value) => {
+  function resetForm() {
+    editUser(initialUser);
+  }
+
+  // An object contiaining an update function for each value in a user
+  const updaters = Object.keys(user).reduce((accumulator, key) => {
+    accumulator[key] = (value) => {
       let newUser = {...user};
       newUser[key] = value;
       editUser(newUser);
     };
-    accumulator.reset[key] = () => {
-      editUser(initialUser);
-    };
     return accumulator;
-  }, {update: {}, reset: {}});
+  }, {});
 
   const pageData = [
     {
       title: `General Information`,
-      body: [
-        {label: `Height`, value: user.height, update: controls.update.height, reset: controls.reset.height},
-        {label: `Weight`, value: user.weight, update: controls.update.weight, reset: controls.reset.weight},
-      ]
+      fields: [
+        {label: `Height`, value: user.height, update: updaters.height},
+        {label: `Weight`, value: user.weight, update: updaters.weight},
+      ],
+      submit: pushUpdate,
+      reset: resetForm,
     },
     {
       title: `Contact Information`,
-      body: [
-        {label: `First Name`, value: user.firstName, update: controls.update.firstName, reset: controls.reset.firstName},
-        {label: `Last Name`, value: user.lastName, update: controls.update.lastName, reset: controls.reset.lastName},
-        {label: `Email`, value: user.email, update: controls.update.email, reset: controls.reset.email},
-      ]
+      fields: [
+        {label: `First Name`, value: user.firstName, update: updaters.firstName},
+        {label: `Last Name`, value: user.lastName, update: updaters.lastName},
+        {label: `Email`, value: user.email, update: updaters.email},
+      ],
+      submit: pushUpdate,
+      reset: resetForm,
     },
   ];
 
   const pageDisplay = pageData.map((card, index) =>
-    <DisplayCard card={card} key={index} onSubmit={pushUpdate}/>
+    <EditableCard key={index} {...card}/>
   );
 
   return (<CardColumns>{pageDisplay}</CardColumns>);
-}
-
-function DisplayCard(props) {
-  const card = props.card;
-
-  const [lock, setLock] = useState(true);
-
-  const editForm = () => {
-    setLock(false);
-  }
-
-  const submitForm = (event) => {
-    setLock(true);
-    props.onSubmit(event);
-  }
-
-  const cancelEdit = () => {
-    card.body.forEach(field => field.reset());
-    setLock(true);
-  }
-
-  return (
-    <Card className="text-center">
-      <Card.Header>{card.title}</Card.Header>
-      <Card.Body>
-        <Form inline={true} onSubmit={props.onSubmit}>
-          {
-            card.body.map((field, index) => 
-              <Form.Group key={index}>
-                <Form.Label column sm={1}>{field.label}</Form.Label>
-                <Col sm={1}>
-                  <Form.Control
-                    readOnly={lock}
-                    value={field.value}
-                    onChange={event => field.update(event.target.value)}
-                  />
-                </Col>
-              </Form.Group>
-            )
-          }
-        </Form>
-      </Card.Body>
-      <Card.Body>
-        <ButtonToolbar>
-          {lock &&
-            <Button variant="secondary" onClick={editForm}>Edit</Button>
-          }
-          {!lock && 
-            <>
-            <Button variant="secondary" onClick={submitForm}>Submit</Button>
-            <Button onClick={cancelEdit}>Cancel</Button>
-            </>
-          }
-        </ButtonToolbar>
-      </Card.Body>
-    </Card>
-  );
 }
