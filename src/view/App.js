afefc7e2feae3f18d01ccc3cd295ead9713dfb3f
H. B. Brooks <hbrooks3>
2019-11-09 22:38:09 -0600

    implemented naive private routing

--- src/view/App.js
+++ src/view/App.js
@@ -3,43 +3,67 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Redirect,
 } from "react-router-dom";
 import Navbar from './Navbar';
 import ProfilePage from "./ProfilePage";
 import HomePage from "./HomePage";
 import PlansPage from "./PlansPage";
 // import CreatePlan from "./CreatePlan";
-import { useSession } from "../dummy-presenter/User";
+import LandingPage from "./LandingPage";
+
+// import { useSession } from "../dummy-presenter/User";
 // import { useSession } from "../presenter/User";
 
 export default function App() {
-  const sessionStatus = useSession();
-
   return (
     <Router>
-      <div>
+      <>
         <Navbar />
-        {sessionStatus ?
-          <Switch>
-            <Route exact path="/">
-              <HomePage />
-            </Route>
-            <Route path="/plans">
-              <PlansPage />
-            </Route>
-            <Route path="/create-Plan">
-              {/* <CreatePlan /> */}
-            </Route>
-            <Route path="/profile">
-              <ProfilePage />
-            </Route>
-            <Route path="/testing">
-              <ProfilePage />
-            </Route>
-          </Switch> :
-          <h1>Login to Access Page</h1>
-        }
-      </div>
+
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <PrivateRoute exact path="/Home">
+            <HomePage />
+          </PrivateRoute>
+          <PrivateRoute path="/plans">
+            <PlansPage />
+          </PrivateRoute>
+          <PrivateRoute path="/create-Plan">
+            {/* <CreatePlan /> */}
+          </PrivateRoute>
+          <PrivateRoute path="/profile">
+            <ProfilePage />
+          </PrivateRoute>
+          <PrivateRoute path="/testing">
+            <ProfilePage />
+          </PrivateRoute>
+        </Switch>
+      </>
     </Router>
+  );
+}
+
+function PrivateRoute({ children, ...rest }) {
+  const sessionStatus = true;
+
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        sessionStatus ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/",
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
   );
 }
