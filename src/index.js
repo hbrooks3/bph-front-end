03c74e03894646c247782f1a526df5a6fba74295
H. B. Brooks <hbrooks3>
2019-11-11 01:06:07 -0600

    added store to app

--- src/index.js
+++ src/index.js
@@ -3,4 +3,21 @@ import ReactDOM from 'react-dom';
 import './custom.scss';
 import App from './view/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// redux
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+import thunkMiddleware from 'redux-thunk';
+import logger from 'redux-logger';
+
+const store = createStore(
+  reducer,
+  applyMiddleware(thunkMiddleware,logger),
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
