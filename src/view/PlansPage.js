7fe59979e09893d2b47bf81a092becddc188f427
H. B. Brooks <hbrooks3>
2019-11-14 18:58:48 -0600

    displays cards

--- src/view/PlansPage.js
+++ src/view/PlansPage.js
@@ -1,9 +1,40 @@
 import React from "react";
 
+// react-bootstrap
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import CardColumns from 'react-bootstrap/CardColumns';
+
+// redux
+import { useSelector, useDispatch } from 'react-redux';
+
+// views
+import LoadingCard from './LoadingCard';
+
 export default function PlansPage(props) {
+  const user = useSelector(state=>state.users[state.auth.uid]);
+  
+  if (!user) {
+    return <LoadingCard />;
+  }
+
+  const cards = user && user.plans.map(plan =>
+    <PlanCard key={plan} planId={plan} />
+  )
+
+  return <CardColumns>{cards}</CardColumns>;
+}
+
+function PlanCard({planId}) {
+  const plan = useSelector(state=>state.plans[planId]);
+
+  if (!plan) {
+    return <LoadingCard />;
+  }
+
   return (
-    <div>
-      <h2>Plans</h2>
-    </div>
+    <Card>
+      {plan.id}
+    </Card>
   );
 }
