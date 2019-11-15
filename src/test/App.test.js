9f2b95887a4e2d4b00e6b09233accba772ec741f
jgriesemer1 <jgriesemer@wisc.edu>
2019-11-15 14:33:03 -0600

    installing enzyme and enzyme-adapter-react-16

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../view/App';
+import Navbar from '../view/Navbar';
+import PlansPage from "../view/PlansPage";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
 
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { MemoryRouter } from 'react-redux';
+import  { MemoryRouter }  from 'react-router-dom';
 import reducer from '../reducers/index.js';
-import thunkMiddleware from 'redux-thunk';
-import logger from 'redux-logger';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+// import thunkMiddleware from 'redux-thunk';
+// import logger from 'redux-logger';
 
 const store = createStore(
   reducer,
 //  applyMiddleware(thunkMiddleware,logger),
 );
-
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
@@ -25,15 +35,33 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(
-//     <Provider store={store}>
-//       <MemoryRouter initialEntries={['App/plans']}>
-//         <App />
-//       </MemoryRouter>
-//     </Provider>,
-//     div
-//   );
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+
+
+Enzyme.configure({ adapter: new Adapter() })
+
+function setup() {
+  const props = {
+    addTodo: jest.fn()
+  }
+
+  const enzymeWrapper = shallow(<App />)
+
+  return {
+    props,
+    enzymeWrapper
+  }
+}
+
+it('should render self and subcomponents', () => {
+  const { enzymeWrapper } = setup()
+
+  //expect(enzymeWrapper.find('app').hasClass('app')).toBe(true)
+  expect(enzymeWrapper.find(Router).containsMatchingElement(Navbar)).toBe(true);
+
+
+  // expect(enzymeWrapper.find('h1').text()).toBe('todos')
+
+  // const todoInputProps = enzymeWrapper.find('TodoTextInput').props()
+  // expect(todoInputProps.newTodo).toBe(true)
+  // expect(todoInputProps.placeholder).toEqual('What needs to be done?')
+})
