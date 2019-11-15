12ec4ca8745b9b4738551f626c69bb3423999bf4
H. B. Brooks <hbrooks3>
2019-11-15 16:19:09 -0600

    added update user

--- src/actions/users.js
+++ src/actions/users.js
@@ -37,14 +37,46 @@ export const fetchUser = (id) => (dispatch) => {
           payload: {id, error: json.error}
         });
       };
-    }).catch(
-      dispatch({
-        type: USER_FETCH_FAILURE,
-        payload: {id, error: 'Failed to load workout'}
-      })
-    )
+    })
   );
 };
+
+const USER_EDIT = 'USER_EDIT';
+
+export const editUser = (user) => (dispatch) => {
+  dispatch({
+    type: USER_EDIT,
+    payload: { id: user.id },
+  });
+
+  return fetch(
+    '/api/User/UpdateUser',
+    {
+      method: 'PUT',
+      body: JSON.stringify({ ...user, userId: user.id }),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    credentials: 'include',
+    }
+  ).then(
+    response => response.json().then(json => {
+      if (response.ok) {
+        const time = Date.now();
+        dispatch({
+          type: USER_FETCH_SUCCESS,
+          payload: {id, time, ...json}
+        });
+      } else {
+        dispatch({
+          type: USER_FETCH_FAILURE,
+          payload: {id, error: json.error}
+        });
+      };
+    })
+  );
+}
 
 const USER_FETCH_DISMISS_ERROR = 'USER_FETCH_DISMISS_ERROR';
 
@@ -55,5 +87,6 @@ const USERS_CLEAR_ALL = 'USERS_CLEAR_ALL';
 export const clearUsers = () => ({type: USERS_CLEAR_ALL});
 
 export {
-  USER_FETCH_ATTEMPT, USER_FETCH_SUCCESS, USER_FETCH_FAILURE, USER_FETCH_DISMISS_ERROR, USERS_CLEAR_ALL
+  USER_FETCH_ATTEMPT, USER_FETCH_SUCCESS, USER_FETCH_FAILURE, USER_FETCH_DISMISS_ERROR, USERS_CLEAR_ALL,
+  USER_EDIT,
 };
