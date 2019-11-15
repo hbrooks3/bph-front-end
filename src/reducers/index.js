2055bd4c9c4c0b79ced305e073546675a9780119
H. B. Brooks <hbrooks3>
2019-11-15 12:49:03 -0600

    added exercises

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,11 @@ import auth from './auth';
 import users from './users';
 import workouts from './workouts';
 import plans from './plans';
+import exercises from './exercises'
 import { combineReducers } from 'redux';
 
 const reducer = combineReducers({
-  auth, users, workouts, plans,
+  auth, users, workouts, plans, exercises,
 });
 
 export default reducer;
