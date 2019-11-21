63edcf5a1c409d9456a9637fc1a9a51ba0aac306
H. B. Brooks <hbrooks3>
2019-11-21 15:23:04 -0600

    fixed error

--- src/test/ExerciseCard.test.js
+++ src/test/ExerciseCard.test.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ExerciseCard from '../view/cards/ExerciseCard.js';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from '../reducers/index.js';
+import {MemoryRouter} from 'react-router-dom';
 
 
-const store = createStore(
-  reducer,
-//  applyMiddleware(thunkMiddleware,logger),
-);
+const store = createStore(reducer);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
     <Provider store={store}>
+      <MemoryRouter initialEntries={["/users/2"]}>
         <ExerciseCard />
-      </Provider>,
+      </MemoryRouter>
+    </Provider>,
     div
   );
   ReactDOM.unmountComponentAtNode(div);
