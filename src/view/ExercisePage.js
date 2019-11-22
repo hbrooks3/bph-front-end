6e7b2f028f496bb013996b18aef74b39d86ea4fc
H. B. Brooks <hbrooks3>
2019-11-22 13:47:41 -0600

    added child creation cards

--- src/view/ExercisePage.js
+++ src/view/ExercisePage.js
@@ -2,9 +2,10 @@
 import React from "react";
 
 // react-bootstrap
+import Card from 'react-bootstrap/Card'
 
 // redux
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 // views
 import ExerciseCard from './cards/ExerciseCard';
@@ -13,17 +14,26 @@ import SetCard from './cards/SetCard';
 // react-router
 import { useParams } from 'react-router-dom';
 
+// actions
+import { addSet } from '../actions/exercises';
+
 export default function ExercisePage(props) {
   const { id } = useParams();
   const exercise = useSelector(state=>state.exercises[id]);
+  const dispatch = useDispatch();
 
-  const cards = (exercise && exercise.exercises &&
+  const cards = (exercise && exercise.sets &&
     exercise.sets.map(set => <SetCard key={set} id={set} footer/>)
   );
   return (
     <>
       <ExerciseCard id={id}/>
       {cards}
+      <Card className="text-center" onClick={()=>dispatch(addSet(id))}>
+        <Card.Body>
+          <h1>Add Set</h1>
+        </Card.Body>
+      </Card>
     </>
   );
 }
--- src/view/PlanPage.js
+++ src/view/PlanPage.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 // react-bootstrap
+import Card from 'react-bootstrap/Card'
 
 // redux
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 // views
 import PlanCard from './cards/PlanCard';
@@ -12,9 +13,13 @@ import WorkoutCard from './cards/WorkoutCard';
 // react-router
 import { useParams } from 'react-router-dom';
 
+// actions
+import { addWorkout } from '../actions/plans';
+
 export default function PlanPage(props) {
   const { id } = useParams();
   const plan = useSelector(state=>state.plans[id]);
+  const dispatch = useDispatch();
 
   const cards = (plan && plan.workouts &&
     plan.workouts.map(plan => <WorkoutCard key={plan} id={plan} footer/>)
@@ -24,6 +29,11 @@ export default function PlanPage(props) {
     <>
       <PlanCard id={id}/>
       {cards}
+      <Card className="text-center" onClick={()=>dispatch(addWorkout(id))}>
+        <Card.Body>
+          <h1>Add Workout</h1>
+        </Card.Body>
+      </Card>
     </>
   );
 }
