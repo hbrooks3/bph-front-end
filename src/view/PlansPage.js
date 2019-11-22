5e5c305ce3c38e695b52d0009563702a21f8eaf4
H. B. Brooks <hbrooks3>
2019-11-21 22:06:38 -0600

    added plan creation card

--- src/view/PlansPage.js
+++ src/view/PlansPage.js
@@ -1,15 +1,20 @@
 import React from "react";
 
 // react-bootstrap
+import Card from "react-bootstrap/Card";
 
 // redux
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 // views
 import PlanCard from './cards/PlanCard';
 
+// actions
+import { addPlan } from '../actions/users';
+
 export default function PlansPage(props) {
   const user = useSelector(state=>state.users[state.auth.uid]);
+  const dispatch = useDispatch();
 
   const cards = (user && user.plans &&
     user.plans.map(plan => <PlanCard key={plan} id={plan} footer />)
@@ -17,6 +22,11 @@ export default function PlansPage(props) {
 
   return (
     <>
+      <Card className="text-center" onClick={()=>dispatch(addPlan(user.id))}>
+        <Card.Body>
+          <h1>Create Plan</h1>
+        </Card.Body>
+      </Card>
       {cards}
     </>
   );
