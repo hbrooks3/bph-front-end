960eda4c191ed9acb7fb82cd59dae735274128bc
H. B. Brooks <hbrooks3>
2019-11-21 22:03:15 -0600

    added needed code

--- src/reducers/plans.js
+++ src/reducers/plans.js
@@ -1,17 +1,110 @@
-const intialPlans = {
-  123: {
-    isFetching: false,
-    isError: false,
-    isLoaded: true,
-    id: '123',
-    trainee: '123',
-    coach: '123',
-    workouts: ['abc','def','ghi']
-  },
+// actions constants
+import { PLAN_GET, PLAN_EDIT, PLAN_ADD_WORKOUT, PLAN_DISSMISS_ERROR, PLANS_CLEAR } from '../actions/plans';
+
+// flag constants
+import { FAILURE, SUCCESS } from '../actions/plans';
+
+const user = (state = {}, action) => {
+  switch (action.type) {
+    case PLAN_GET:
+      switch (action.flag) {
+        case SUCCESS:
+          return {
+            ...state,
+            loading: false,
+            error: false,
+            loaded: true,
+            workouts: [],
+            ...action.payload,
+          }
+        case FAILURE:
+          return {
+            ...state,
+            loading: false,
+            error: true,
+            loaded: false,
+            errorMessage: action.payload
+          }
+        default:
+          return {
+            ...state,
+            id: action.id,
+            loading: true,
+            error: false,
+            loaded: false,
+          }
+      }
+    case PLAN_EDIT:
+      switch (action.flag) {
+        case SUCCESS:
+          return {
+            ...state,
+            loading: false,
+            error: false,
+            ...action.payload,
+          }
+        case FAILURE:
+          return {
+            ...state,
+            loading: false,
+            error: true,
+            errorMessage: action.payload
+          }
+        default:
+          return {
+            ...state,
+            loading: true,
+            error: false,
+          }
+      }
+    case PLAN_ADD_WORKOUT:
+      switch (action.flag) {
+        case SUCCESS:
+          return {
+            ...state,
+            loading: false,
+            error: false,
+            workouts: [...state.workouts, action.payload.workoutId]
+          }
+        case FAILURE:
+          return {
+            ...state,
+            loading: false,
+            error: true,
+            errorMessage: action.payload
+          }
+        default:
+          return {
+            ...state,
+            loading: true,
+            error: false,
+          }
+      }
+    case PLAN_DISSMISS_ERROR:
+      return {
+        ...state,
+        error: false,
+      }
+    default:
+      return state;
+  };
 };
 
-const plans = (state = intialPlans, action) => {
-  return state
-}
+const users = (state = {}, action) => {
+  switch (action.type) {
+    case PLAN_EDIT:
+    case PLAN_GET:
+    case PLAN_ADD_WORKOUT:
+    case PLAN_DISSMISS_ERROR:
+      return {
+        ...state,
+        [action.id]: user(state[action.id], action),
+      };
+    case PLANS_CLEAR:
+      return {};
+    default:
+      return state;
+  };
+};
 
-export default plans;
+export default users;
