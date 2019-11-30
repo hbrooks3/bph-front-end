36dbfb5f2b76c2879390f32f1c844f76d72d3ba1
H. B. Brooks <hbrooks3>
2019-11-29 18:18:59 -0600

    Added editable portion

--- src/view/cards/PlanCard.js
+++ src/view/cards/PlanCard.js
@@ -1,63 +1,132 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // react-bootstrap
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+
 // redux
 import { useSelector, useDispatch } from 'react-redux';
 
 // views
-import FetchingCard from './FetchingCard'
+import FetchingCard from './FetchingCard';
 
 // react-router
 import { useHistory } from 'react-router-dom';
 
 // actions
 import { getPlan, dissmissPlanError } from '../../actions/plans';
+import { getUser } from '../../actions/users';
 
-export default function PlanCard({id, footer=false, editable=false}) {
+export default function PlanCard({id, preview=false, editable=false}) {
   const plan = useSelector(state=>state.plans[id]);
   const dispatch = useDispatch();
   const history = useHistory();
 
-  return (
-    <Card onClick={footer ? ()=>history.push(`/plan/${id}`) : null}>
+  if (!(plan && plan.loaded)) {
+    return (
+      <FetchingCard
+        id={id}
+        type='plans'
+        fetch={()=>dispatch(getPlan(id))}
+        dismissError={()=>dispatch(dissmissPlanError(id))}
+      />
+    );
+  }
 
-      {(!plan || !plan.loaded) &&
-        <FetchingCard
-          id={id}
-          type='plans'
-          fetch={()=>dispatch(getPlan(id))}
-          dismissError={()=>dispatch(dissmissPlanError(id))}
-        />
-      }
-      {plan && plan.loaded &&
-        <>
-        <CardBody plan={plan}/>
-        </>
-      }      
+  const coach = Name({uid: plan.coachId});
+  const trainee = Name({uid: plan.traineeId});
+
+  if (preview) {
+    return (
+      <Card onClick={()=>history.push(`/plan/${id}`)}>
+        <Card.Body>
+          <Card.Title>Plan</Card.Title>
+          <Card.Text>Coach: {coach}</Card.Text>
+          <Card.Text>Trainee: {trainee}</Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  }
+
+  if (editable) {
+    return (
+      <EditablePlanCard coach={coach} trainee={trainee}/>
+    );
+  }
+
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>Plan</Card.Title>
+        <Card.Text>Coach: {coach}</Card.Text>
+        <Card.Text>Trainee: {trainee}</Card.Text>
+      </Card.Body>
     </Card>
   );
 }
 
-const getName = uid => state => state.users[uid] ? `${state.users[uid].firstName} ` : 'Not Assigned'
+function Name({uid}) {
+  const user = useSelector(state => state.users[uid]);
+  const [name, setName] = useState('Loading...');
+  const dispatch = useDispatch();
 
-// function Name({uid}) {
-//   if (!uid) {
+  useEffect(() => {
+    if (!uid) {
+      setName('Not Assigned');
+    } else if (user && user.loaded) {
+      setName(`${user.firstName} ${user.lastName}`);
+    } else if (!user || !user.loading) {
+      dispatch(getUser(uid));
+    }
+  }, [uid, user, dispatch]);
 
-//   }
+  return name;
+}
 
-//   return '';
-// }
+function EditablePlanCard({coach, trainee}) {
+  const [lock, setLock] = useState(true);
 
-function CardBody({plan}) {
-  const coach = useSelector(getName(plan.coachId));
-  const trainee = useSelector(getName(plan.traineeId));
+  if (lock) {
+    return (
+      <Card>
+        <Card.Body>
+          <Card.Title>Plan</Card.Title>
+          <Card.Text>Coach: {coach}</Card.Text>
+          <Card.Text>Trainee: {trainee}</Card.Text>
+        </Card.Body>
+        <Card.Body>
+          <Button onClick={()=>setLock(false)}>Edit</Button>
+        </Card.Body>
+      </Card>
+    )
+  }
 
   return (
-    <Card.Body>
-      <Card.Title>Plan</Card.Title>
-      <Card.Text>Coach: {coach}</Card.Text>
-      <Card.Text>Trainee: {trainee}</Card.Text>
-    </Card.Body>
+    <Card>
+      <Card.Body>
+        <Card.Title>Plan</Card.Title>
+        <Card.Text>Coach: {coach}</Card.Text>
+        <Form inline>
+          <Form.Row>
+            <Col>
+              <Form.Label as='card-text'>Trainee: </Form.Label>
+            </Col>
+
+            <Col>
+              <Form.Control as='select'>
+                <option>{trainee}</option>
+                <option>Other trainee</option>
+                <option>This needs to be done</option>
+              </Form.Control>
+            </Col>
+          </Form.Row>
+        </Form>
+      </Card.Body>
+      <Card.Body>
+        <Button onClick={()=>setLock(true)}>Cancel</Button>
+      </Card.Body>
+    </Card>
   );
 }
