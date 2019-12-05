9d59ddd8c8ba4d05909da1788281e852ec9451b7
H. B. Brooks <hbrooks3>
2019-12-05 11:41:13 -0600

    correctly implemented ErrorAlert test

--- src/view/ErrorAlert.test.js
+++ src/view/ErrorAlert.test.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ErrorAlert from './ErrorAlert.js';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import reducer from '../reducers/index.js';
-
-
-const store = createStore(
-  reducer,
-//  applyMiddleware(thunkMiddleware,logger),
-);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
-    <Provider store={store}>
-        <ErrorAlert />
-      </Provider>,
+    <ErrorAlert
+      heading='There was an error!'
+      message='You messed up dude.'
+      callback={()=>console.log('Error alert closed')}
+    />,
     div
   );
   ReactDOM.unmountComponentAtNode(div);
